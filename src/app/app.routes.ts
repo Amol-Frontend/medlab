e532a4753459dc932407275bfb931ce1cee37d81
amol <amolfrontend@gmail.com>
2025-10-04 09:40:33 +0530

    page-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : 'home' , component :HomeComponent},
+    { path : 'medicine-home', component : MedicineHomeComponent},
+    { path : '', redirectTo : '/home', pathMatch : 'full'}
+];
---
+++ src/app/components/medicine-home/medicine-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicine-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './medicine-home.component.html',
+  styleUrl: './medicine-home.component.css'
+})
+export class MedicineHomeComponent {
+
+}
